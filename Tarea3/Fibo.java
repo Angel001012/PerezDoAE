a6595f7947a30d2ad97fd523f34c600b0e6eccbe
unknown <perezangel2015@gmail>
2020-09-13 16:21:33 -0500

    Commit Terminado

--- Tarea3/Fibo.java
+++ Tarea3/Fibo.java
@@ -1,5 +1,18 @@
 class Fibo{
-    public static void public static void main(String[] args) {
-        
-    }
+    public static void main(String args[]){
+        int Valor1=0;
+        int Valor2=1;
+        int Valor3;
+        int i=1;
+        System.out.println(Valor1);
+        System.out.println(Valor2);
+        do
+        {
+            Valor3=Valor1+Valor2;
+            System.out.println(Valor3);
+            Valor1=Valor2;
+            Valor2=Valor3;
+            i=i+1;
+        }while(i<10);
+    } 
 }
